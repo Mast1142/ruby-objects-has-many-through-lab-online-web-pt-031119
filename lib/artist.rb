169f9aadcc02726762f19623659855a9ac7bfd46
Samuel T <ssamt1142@gmail.com>
2019-04-21 14:53:25 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist
+  attr_accessor :name, :artist, :genre
+  attr_reader
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    Song.new(name, genre)
+    Song.artist = self
+  end
+
+  def songs
+    Songs.all.select do |x|
+      songs.artist == self
+    end
+  end
+
+  def genres
+    Songs.map do |x|
+      x.genres
+    end
+  end
+end
