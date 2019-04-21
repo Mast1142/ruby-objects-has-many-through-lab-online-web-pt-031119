96c97285c1e948e5daaad6866c7c731531149be4
Samuel T <ssamt1142@gmail.com>
2019-04-21 17:06:57 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,7 +2,10 @@ class Appointment
   attr_accessor :name
   @@all = []
 
-  def initialize
+  def initialize(name, date, doctor)
+    @name = name
+    @date = date
+    @doctor = doctor
   end
 
   def self.all
